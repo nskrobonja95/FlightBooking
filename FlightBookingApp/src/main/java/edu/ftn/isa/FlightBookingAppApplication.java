7890756e096621a83d24155af523283ba6408f06
neven95 <neven95jovic@gmail.com>
2018-11-08 18:22:32 +0100

    Starter project

---
+++ FlightBookingApp/src/main/java/edu/ftn/isa/FlightBookingAppApplication.java
@@ -0,0 +1,12 @@
+package edu.ftn.isa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingAppApplication.class, args);
+	}
+}
